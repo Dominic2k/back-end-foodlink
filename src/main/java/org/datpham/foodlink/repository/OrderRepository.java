425fbe1e48315ca2055af9cd64321f6318b8cc00
nguyenluongthong1706-hash <nguyenluongthong1706@gmail.com>
2026-03-15 13:22:03 +0700

    feat: [FL-32] view order detail

--- src/main/java/org/datpham/foodlink/repository/OrderRepository.java
+++ src/main/java/org/datpham/foodlink/repository/OrderRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
     Page<Order> findByStatus(Order.OrderStatus status, Pageable pageable);
     Page<Order> findByUserEmailOrderByCreatedAtDesc(String email, Pageable pageable);
+    Optional<Order> findByIdAndUserEmail(String id, String email);
     long countByStatus(Order.OrderStatus status);
 }
